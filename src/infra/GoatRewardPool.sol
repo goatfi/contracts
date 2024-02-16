da6ff9890da761e34b55777b47684fdf7a352fa2
reikodoteth <149171651+reikodoteth@users.noreply.github.com>
2024-02-16 16:37:21 +0100

    Fix typo

--- src/infra/GoatRewardPool.sol
+++ src/infra/GoatRewardPool.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.20;
 import { SafeERC20, IERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
-import { TrustlessPermit } from "@trust-security/trustlesPermit/TrustlessPermit.sol";
+import { TrustlessPermit } from "@trust-security/trustlessPermit/TrustlessPermit.sol";
 import { Math } from "@openzeppelin/contracts/utils/math/Math.sol";
 
 /// @notice Multi-reward staking contract for GOA
