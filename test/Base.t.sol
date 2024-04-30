175621708f6dccd596656a5f88eede3d716a87d3
0xtjoe <leon.lowden@hotmail.com>
2024-04-30 12:34:07 +1000

    feat: update keeper of feeconfigurator

--- test/Base.t.sol
+++ test/Base.t.sol
@@ -2,9 +2,29 @@
 pragma solidity 0.8.20;
 
 import { Test } from "forge-std/Test.sol";
+import { Users } from "./Utils/Types.sol";
 
 abstract contract Base_Test is Test {
+    /*//////////////////////////////////////////////////////////////////////////
+                                     VARIABLES
+    //////////////////////////////////////////////////////////////////////////*/
+
+    Users internal users;
+
     function setUp() public virtual {
-        
+        // Create users for testing.
+        users = Users({
+            owner: createUser("Owner"),
+            keeper: createUser("Keeper"),
+            alice: createUser("Alice"),
+            bob: createUser("Bob")
+        });
+    }
+
+    /// @dev Generates a user, labels its address, and funds it with test assets.
+    function createUser(string memory name) internal returns (address payable) {
+        address payable user = payable(makeAddr(name));
+        vm.deal({account: user, newBalance: 100 ether});
+        return user;
     }
 }
--- test/Unit/concrete/FeeConfigurator/FeeConfigurator.t.sol
+++ test/Unit/concrete/FeeConfigurator/FeeConfigurator.t.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.20;
 import { Base_Test } from "../../../Base.t.sol";
 import { FeeConfigurator } from "src/infra/FeeConfigurator.sol";
 
-contract FeeConfiguratorTest is Base_Test {
+contract FeeConfigurator_Unit_Concrete_Test is Base_Test {
     /*//////////////////////////////////////////////////////////////////////////
                                    TEST CONTRACTS
     //////////////////////////////////////////////////////////////////////////*/
@@ -19,7 +19,7 @@ contract FeeConfiguratorTest is Base_Test {
         super.setUp();
 
         feeConfig = new FeeConfigurator();
-        feeConfig.initialize(address(this), 0.05 ether);
+        feeConfig.initialize(users.keeper, 0.05 ether);
 
         vm.label({
             account: address(feeConfig),
