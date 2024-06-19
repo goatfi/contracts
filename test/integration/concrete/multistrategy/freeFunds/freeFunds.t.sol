b9745bb51d9946e051472528a14bbed45d2070d6
reikodoteth <149171651+reikodoteth@users.noreply.github.com>
2024-06-19 13:31:51 +0200

    freeFunds tests

--- test/integration/concrete/multistrategy/freeFunds/freeFunds.t.sol
+++ test/integration/concrete/multistrategy/freeFunds/freeFunds.t.sol
@@ -1,8 +1,47 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.20 <0.9.0;
 
-import { Multistrategy_Integration_Shared_Test } from "../../../shared/Multistrategy.t.sol";
+import { MultistrategyHarness_Integration_Shared_Test } from "../../../shared/MultistrategyHarness.t.sol";
 
-contract FreeFunds_Integration_Concrete_Test is Multistrategy_Integration_Shared_Test {
-    
+contract FreeFunds_Integration_Concrete_Test is MultistrategyHarness_Integration_Shared_Test {
+    function test_FreeFunds_ZeroTotalAssets() external {
+        // Assert that free funds is zero when total Assets is zero
+        uint256 actualFreeFunds = multistrategyHarness.freeFunds();
+        uint256 expectedFreeFunds = 0;
+        assertEq(actualFreeFunds, expectedFreeFunds, "freeFunds");
+    }
+
+    modifier whenTotalAssetsNotZero() {
+        _;
+    }
+
+    function test_FreeFunds_ZeroLockedProfit() 
+        external
+        whenTotalAssetsNotZero
+    {
+        uint256 totalAssets = multistrategyHarness.totalAssets();
+
+        // Assert that free funds is totalAssets when locked profit is 0
+        uint256 actualFreeFunds = multistrategyHarness.freeFunds();
+        uint256 expectedFreeFunds = totalAssets;
+        assertEq(actualFreeFunds, expectedFreeFunds, "freeFunds");
+    }
+
+    modifier whenLockedProfitNotZero() {
+        _;
+    }
+
+    function test_FreeFunds()
+        external
+        whenTotalAssetsNotZero
+        whenLockedProfitNotZero 
+    {
+        uint256 totalAssets = multistrategyHarness.totalAssets();
+        uint256 lockedProfit = multistrategyHarness.calculateLockedProfit();
+
+        // Assert that free funds is totalAssets minus locked profit
+        uint256 actualFreeFunds = multistrategyHarness.freeFunds();
+        uint256 expectedFreeFunds = totalAssets - lockedProfit;
+        assertEq(actualFreeFunds, expectedFreeFunds, "freeFunds");
+    }
 }
