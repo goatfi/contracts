dada9b7d487c19b398fdc71ecb1c81a6d6e89ee4
reikodoteth <149171651+reikodoteth@users.noreply.github.com>
2024-07-08 14:58:18 +0200

    Deposit and Withdraw .tree files

---
+++ test/integration/concrete/multistrategy/withdraw/withdraw.t.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.20 <0.9.0;
+
+import { Multistrategy_Integration_Shared_Test } from "../../../shared/Multistrategy.t.sol";
+import { IStrategyAdapter } from "interfaces/infra/multistrategy/IStrategyAdapter.sol";
+
+contract Withdraw_Integration_Concrete_Test is Multistrategy_Integration_Shared_Test {
+    
+}
