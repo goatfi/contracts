f658095d8119900a486c63c590b522198b206bcb
reikodoteth <149171651+reikodoteth@users.noreply.github.com>
2024-07-11 23:18:24 +0200

    StrategyReport and ReqeustCredit .tree files

---
+++ test/integration/concrete/multistrategy/strategyReport/strategyReport.t.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.20 <0.9.0;
+
+import { Multistrategy_Integration_Shared_Test } from "../../../shared/Multistrategy.t.sol";
+import { IStrategyAdapter } from "interfaces/infra/multistrategy/IStrategyAdapter.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { Errors } from "src/infra/libraries/Errors.sol";
+
+contract StrategyReport_Integration_Concrete_Test is Multistrategy_Integration_Shared_Test {
+    
+}
