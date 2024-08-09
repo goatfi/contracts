0be61d5d5cc00c78c77b13850515b0954835e3de
reikodoteth <149171651+reikodoteth@users.noreply.github.com>
2024-08-09 12:38:17 +0200

    Echidna ERC4626 properties

---
+++ test/integration/fuzz/ERC4626/MultistrategyERC4626Properties.t.sol
@@ -0,0 +1,15 @@
+    import {CryticERC4626PropertyTests} from "properties/ERC4626/ERC4626PropertyTests.sol";
+    // this token _must_ be the vault's underlying asset
+    import {ERC20Mock} from "../../../mocks/erc20/ERC20Mock.sol";
+    // change to your vault implementation
+    import {Multistrategy} from "src/infra/multistrategy/Multistrategy.sol";
+
+    contract MultistrategyERC4626Harness is CryticERC4626PropertyTests {
+        address manager = address(5);
+        address feeRecipient = address(6);
+        constructor () {
+            ERC20Mock _asset = new ERC20Mock("DAI", "DAI");
+            Multistrategy _vault = new Multistrategy(address(_asset), manager, feeRecipient, "Goat DAI", "gDAI");
+            initialize(address(_vault), address(_asset), false);
+        }
+    }
