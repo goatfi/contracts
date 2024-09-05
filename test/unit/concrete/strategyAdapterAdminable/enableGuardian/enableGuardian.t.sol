6dc4d3929e9630d718fabd6fa99f5195912b04b1
reikodoteth <149171651+reikodoteth@users.noreply.github.com>
2024-09-05 17:09:02 +0200

    Upgrade solc to 0.8.27

--- test/unit/concrete/strategyAdapterAdminable/enableGuardian/enableGuardian.t.sol
+++ test/unit/concrete/strategyAdapterAdminable/enableGuardian/enableGuardian.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.20;
+pragma solidity ^0.8.20;
 
 import { StrategyAdapter_Unit_Shared_Test } from "../../../shared/StrategyAdapter.t.sol";
 import { IStrategyAdapterAdminable } from "interfaces/infra/multistrategy/IStrategyAdapterAdminable.sol";
