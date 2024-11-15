22c9b8b5bd0544e14bc3e30b93ae1f975a010f06
reikodoteth <149171651+reikodoteth@users.noreply.github.com>
2024-11-15 16:40:33 +0100

    Add missing tests

--- test/shared/MultistrategyHarness.sol
+++ test/shared/MultistrategyHarness.sol
@@ -5,9 +5,13 @@ import { Multistrategy } from "src/infra/multistrategy/Multistrategy.sol";
 import { IMultistrategy } from "interfaces/infra/multistrategy/IMultistrategy.sol";
 
 interface IMultistrategyHarness is IMultistrategy {
+    function profitUnlockTime() external view returns (uint256);
+    function calculateLockedProfit() external view returns(uint256);
     function liquidity() external view returns(uint256);
+    function freeFunds() external view returns(uint256);
     function reportLoss(address _strategy, uint256 _loss) external;
 }
+
 
 /// @dev This contract exposes the internal functions of the multistrategy contract.
 /// ONLY TO BE USED FOR TESTING
@@ -27,11 +31,23 @@ contract MultistrategyHarness is IMultistrategyHarness, Multistrategy {
             _symbol
         ) {}
 
+    function calculateLockedProfit() external view returns(uint256) {
+        return _calculateLockedProfit();
+    }
+
     function liquidity() external view returns(uint256) {
         return _liquidity();
     }
 
+    function freeFunds() external view returns(uint256) {
+        return _freeFunds();
+    }
+
     function reportLoss(address _strategy, uint256 _loss) external {
         _reportLoss(_strategy, _loss);
+    }
+
+    function profitUnlockTime() external pure returns (uint256) {
+        return PROFIT_UNLOCK_TIME;
     }
 }
