ba765e8642a4753f02f76627de3d6397ce7ddb56
reikodoteth <149171651+reikodoteth@users.noreply.github.com>
2025-01-14 17:01:32 +0100

    StargateAdapter invariants

--- test/invariant/adapters/StargateAdapterInvariants.t.sol
+++ test/invariant/adapters/StargateAdapterInvariants.t.sol
@@ -9,47 +9,42 @@ import { AdapterHandler } from "./AdapterHandler.t.sol";
 import { AssetsArbitrum } from "@addressbook/AssetsArbitrum.sol";
 import { ProtocolArbitrum } from "@addressbook/ProtocolArbitrum.sol";
 import { Users } from "../../utils/Types.sol";
-import { SiloAdapter } from "src/infra/multistrategy/adapters/SiloAdapter.sol";
 import { StrategyAdapterHarvestable } from "src/abstracts/StrategyAdapterHarvestable.sol";
-import { IStrategyAdapterHarvestable } from "interfaces/infra/multistrategy/IStrategyAdapterHarvestable.sol";
+import { StargateAdapter } from "src/infra/multistrategy/adapters/StargateAdapter.sol";
 
-
-contract SiloAdapterInvariants is AdapterInvariantBase {
+contract StargateAdapterInvariants is AdapterInvariantBase {
     AdapterHandler handler;
-    address asset = AssetsArbitrum.WETH;
+    address asset = AssetsArbitrum.USDC;
 
     function setUp() public {
         vm.createSelectFork(vm.envString("ARBITRUM_RPC_URL"));
 
         createUsers();
         handler = new AdapterHandler(
-            createMultistrategy(asset, 1_000 * (10 ** IERC20Metadata(asset).decimals())), 
+            createMultistrategy(asset, 1_000_000 * (10 ** IERC20Metadata(asset).decimals())), 
             createAdapter(), 
             users,
-            true
+            false
         );
 
-        makeInitialDeposit(0.01 ether);
+        makeInitialDeposit(10 * (10 ** IERC20Metadata(asset).decimals()));
         targetContract(address(handler));
     }
 
-    function createAdapter() public returns (SiloAdapter) {
-        SiloAdapter.SiloAddresses memory siloAddresses = SiloAdapter.SiloAddresses({
-            silo: 0x1182559e5cf2247e4DdB7a38e28a88ec3825f2BA,
-            collateral: 0x95633979ae07b857a5A03BbA349EAE891E27fB5E,
-            siloLens: 0xBDb843c7a7e48Dc543424474d7Aa63b61B5D9536,
-            siloRewards: 0xbDBBf747402653A5aD6F6B8c49F2e8dCeC37fAcF,
-            merklDistributor: 0x3Ef3D8bA38EBe18DB133cEc108f4D14CE00Dd9Ae
-        });
-
+    function createAdapter() public returns (StargateAdapter) {
         StrategyAdapterHarvestable.HarvestAddresses memory harvestAddresses = StrategyAdapterHarvestable.HarvestAddresses({
             swapper: ProtocolArbitrum.GOAT_SWAPPER,
             weth: AssetsArbitrum.WETH
         });
 
-        SiloAdapter adapter = new SiloAdapter(address(multistrategy), multistrategy.asset(), harvestAddresses, siloAddresses, "", "");
+        StargateAdapter.StargateAddresses memory stargateAddresses = StargateAdapter.StargateAddresses({
+            router: 0xe8CDF27AcD73a434D661C84887215F7598e7d0d3,
+            chef: 0x3da4f8E456AC648c489c286B99Ca37B666be7C4C
+        });
+
+        StargateAdapter adapter = new StargateAdapter(address(multistrategy), multistrategy.asset(), harvestAddresses, stargateAddresses, "", "");
         adapter.transferOwnership(users.keeper);
-        vm.prank(users.keeper); adapter.addReward(AssetsArbitrum.SILO);
+        vm.prank(users.keeper); adapter.addReward(0x6694340fc020c5E6B96567843da2df01b2CE1eb6); // STG
         vm.prank(users.keeper); adapter.enableGuardian(users.guardian);
         vm.prank(users.owner); multistrategy.addStrategy(address(adapter), 10_000, 0, type(uint256).max);
 
@@ -62,7 +57,7 @@ contract SiloAdapterInvariants is AdapterInvariantBase {
         console.log("Yield Time:", handler.ghost_yieldTime());
 
         if(handler.ghost_yieldTime() > 0 && handler.ghost_deposited() > 0) {
-            assertGt(multistrategy.pricePerShare(), 1 ether);
+            assertGe(multistrategy.pricePerShare(), 1 * (10 ** IERC20Metadata(asset).decimals()));
         }
     }
 }
